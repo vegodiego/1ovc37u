9c22ebc1c07aa3cebc51260e1813480c9c44a5b4
Germán Escobar <german.escobarc@gmail.com>
2018-09-08 11:21:34 -0500

    Complete first working version

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
+const md = require('marked');
 
 const app = express();
 
@@ -9,11 +11,18 @@ app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 const NoteSchema = new mongoose.Schema({
   title: { type: String },
   body: { type: String }
 });
+NoteSchema.methods.truncateBody = function() {
+  if (this.body && this.body.length > 75) {
+    return this.body.substring(0, 70) + " ...";
+  }
+  return this.body;
+};
 const Note = mongoose.model("Note", NoteSchema);
 
 app.get("/", async (req, res) => {
@@ -21,8 +30,9 @@ app.get("/", async (req, res) => {
   res.render("index",{ notes: notes } )
 });
 
-app.get("/notes/new", (req, res) => {
-  res.render("new");
+app.get("/notes/new", async (req, res) => {
+  const notes = await Note.find();
+  res.render("new", { notes: notes });
 });
 
 app.post("/notes", async (req, res, next) => {
@@ -39,6 +49,39 @@ app.post("/notes", async (req, res, next) => {
   }
 
   res.redirect('/');
+});
+
+app.get("/notes/:id", async (req, res) => {
+  const notes = await Note.find();
+  const note = await Note.findById(req.params.id);
+  res.render("show", { notes: notes, currentNote: note, md: md });
+});
+
+app.get("/notes/:id/edit", async (req, res, next) => {
+  const notes = await Note.find();
+  const note = await Note.findById(req.params.id);
+  res.render("edit", { notes: notes, currentNote: note });
+});
+
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const note = await Note.findById(id);
+
+  note.title = req.body.title;
+  note.body = req.body.body;
+
+  try {
+    await note.save();
+  } catch (e) {
+    return next(e);
+  }
+
+  res.status(204).send({});
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  await Note.deleteOne({ _id: req.params.id });
+  res.status(204).send({});
 });
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
